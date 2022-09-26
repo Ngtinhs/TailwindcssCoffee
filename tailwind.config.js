751785bf61fe593c8f5f070bd24b3b8a7e778c3c
ngtinh <nguyenvanductinh77@gmail.com>
2022-09-27 00:52:43 +0700

    Story Section & SubHeading

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
         Karla: ['Karla', 'sans-serif']
       },
       colors: {
-        'light-coffee': '#C89F94'
+        'light-coffee': '#C89F94',
+
+        //Có thể làm ntn
+        'coffee': {
+          50: '#e8d6d0',
+          200: '#c89f94',
+          400: '#a25f4b',
+          600: '#744838'
+        }
       },
       keyframes: {
         slideDown: {
