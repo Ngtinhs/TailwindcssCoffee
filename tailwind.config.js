d64ca49ddfc181650d5b45c0c17d016b4196b24e
ngtinh <nguyenvanductinh77@gmail.com>
2022-09-27 11:22:29 +0700

    Featured Mugs & Product Card

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,10 +21,12 @@ module.exports = {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
-        }
+        },
+        fadeIn: { from: { opacity: 0 }, to: { opacity: 1 } }
       },
       animation: {
-        slideDown: 'slideDown .4s ease-in-out',
+        slideDown: 'slideDown .5s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out',
       }
     },
   },
